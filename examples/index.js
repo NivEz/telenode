301b47c98b1a395ca1eee7f26c2de922989ecbf7
NivEz <niv.cyber@gmail.com>
2023-01-20 19:19:15 +0200

    added error that explains how to use --file argument in dev script

--- examples/index.js
+++ examples/index.js
@@ -3,4 +3,12 @@
  * Just pass --file to the package.json run dev command like so: `npm --file=hello run dev`
  */
 
-require(`./${process.env.npm_config_file}`);
+const exampleFileArg = process.env.npm_config_file;
+
+if (exampleFileArg) {
+	require(`./${exampleFileArg}`);
+} else {
+	const errorMessage = '`--file=<example name from examples>` argument is required. For example: npm --file=hello run dev';
+	throw Error(errorMessage);
+}
+
